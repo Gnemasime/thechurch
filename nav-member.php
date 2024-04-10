6c960a8f0a95b67b591859f8f18d8d837f26ce90
Gnemasime <158264023+Gnemasime@users.noreply.github.com>
2024-04-10 20:16:41 +0200

    Add files via upload

---
+++ nav-member.php
@@ -0,0 +1,46 @@
+<nav class="site-nav">
+		<div class="container">
+			<div class="site-navigation">
+				<a href="index.php" class="logo m-0 float-left">Holy Rosary Church</a>
+
+				<ul class="js-clone-nav d-none d-lg-inline-block text-start site-menu float-end">
+					<li><a href="index.php">Home</a></li>
+					
+					<li class="has-children"><a href="#">Dashboard</a>
+					<ul class="dropdown">
+					<li><a href="members/index.php">Member</a></li>
+					<li><a href="members/member_log.php">Admin</a></li>
+</ul>
+							
+							<li>
+								<a href="ecomm/index.php">Shop</a>
+								
+						
+							</li>
+						
+					</li>
+					
+					
+					
+					<li><a href="contact.php">Contact</a></li>
+					
+					<li class="has-children cta-button"><a href="#">Join us / Login</a>
+					<ul class ="dropdown">
+						<li><a href="cman/index.php">Member</a></li>
+						<li><a href="ecomm/index.php">Visitor</a></li>
+						<li><a href="cman/admin/index.php">Admin</a></li>
+	
+					</ul></li>
+					
+				</ul>
+
+				
+
+				<a href="#" class="burger ms-auto float-end site-menu-toggle js-menu-toggle d-inline-block d-lg-none light" data-toggle="collapse" data-target="#main-navbar">
+					<span></span>
+				</a>
+
+				
+			</div>
+		</div>
+	</nav>
---
+++ shop.php
@@ -0,0 +1,193 @@
+
+<!doctype html>
+<html lang="en">
+<head>
+	<!-- Required meta tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	
+	<meta name="author" content="Untree.co">
+	<link rel="shortcut icon" href="favicon.png">
+
+	<meta name="description" content="" />
+	<meta name="keywords" content="bootstrap, bootstrap5" />
+	
+
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Cinzel:wght@400;600&family=Inter&display=swap" rel="stylesheet">
+
+
+	<link rel="stylesheet" href="fonts/icomoon/style.css">
+	<link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
+
+	<link rel="stylesheet" href="css/tiny-slider.css">
+	<link rel="stylesheet" href="css/glightbox.min.css">
+	<link rel="stylesheet" href="css/aos.css">
+	<link rel="stylesheet" href="css/style.css">
+
+	<title>Holy Rosary Church</title>
+</head>
+<body >
+
+	<div class="site-mobile-menu site-navbar-target">
+		<div class="site-mobile-menu-header">
+			<div class="site-mobile-menu-close">
+				<span class="icofont-close js-menu-toggle"></span>
+			</div>
+		</div>
+		<div class="site-mobile-menu-body"></div>
+	</div>
+
+	<nav class="site-nav">
+		<div class="container">
+			<div class="site-navigation">
+				<a href="../index.php" class="logo m-0 float-left">Holy Rosary Church</a>
+
+				<ul class="js-clone-nav d-none d-lg-inline-block text-start site-menu float-end">
+					<li><a href="../index.php">Home</a></li>
+                    <li class="has-children"><a href="#">Dashboard</a>
+					<ul class="dropdown">
+					<li><a href="index.php">Member</a></li>
+					<li><a href="member_log.php">Admin</a></li>
+</ul>
+					<li >
+						<a href="../ecomm/index.php">Shop</a>
+						
+							
+						
+					
+								
+						
+							</li>
+						
+					</li>
+					
+					
+					
+					<li><a href="../contact.php">Contact</a></li>
+					
+					<li class="has-children cta-button"><a href="#">Join us / Login</a>
+					<ul class ="dropdown">
+						<li><a href="../cman/index.php">Member</a></li>
+						<li><a href="../ecomm/index.php">Visitor</a></li>
+						<li><a href="../cman/admin/index.php">Admin</a></li>
+					</ul></li>
+					
+					
+				</ul>
+
+				
+
+				<a href="#" class="burger ms-auto float-end site-menu-toggle js-menu-toggle d-inline-block d-lg-none light" data-toggle="collapse" data-target="#main-navbar">
+					<span></span>
+				</a>
+
+				
+			</div>
+		</div>
+	</nav>
+	
+	
+	<div class="hero page-inner overlay" style="background-image: url('../images/landscape-1.jpg'); ">
+
+		<div class="container">
+			<div class="row justify-content-center align-items-center">
+				<div class="col-lg-9 text-center mt-5">
+
+					<h1 class="heading" data-aos="fade-up">Church Member</h1>
+					<p class="text-white-50 mb-5" data-aos="fade-up" data-aos-delay="100"></p>
+					<nav aria-label="breadcrumb" data-aos="fade-up" data-aos-delay="200">
+						<ol class="breadcrumb text-center justify-content-center">
+							<li class="breadcrumb-item "><a href="#">Home</a></li>
+							<li class="breadcrumb-item "><a href="#">Member</a></li>
+							<li class="breadcrumb-item active text-white-50" aria-current="page">Church Member Dashboard </li>
+						</ol>
+					</nav>
+
+				</div>
+			</div>
+
+
+		</div>
+	</div>
+
+    <div class="section">
+		<div class="container">
+			<div class="row mb-4 text-center">
+				<div class="col-lg-12" data-aos="fade-up">
+					<span class="subheading d-block mb-4">Welcome / Siyakwamukela / Dumelang / Molo  </span>
+					<h2 class="mb-4 heading">Categories</h2>
+				</div>
+    <div class="row" data-aos="fade-up" >
+
+			</div>
+			<div class="row g-5">
+				<div class="col-12 col-sm-6 col-md-6 col-lg-4" data-aos="fade-up" data-aos-delay="0">
+					<div class="sermon-entry">
+						<a href="sermon-single.html"><img src="images/sq-3.jpg" alt="Image" class="img-fluid mb-3 rounded"></a>
+						<div class="sermon-body">
+							<span class="date">15 Jan 2020 <span class="mx-2">&bullet;</span> By Pastor Campbell </span>
+							<h3 class="mb-2"><a href="#">Living and Sharing The Gospel</a></h3>
+							<p class="mb-5">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia</p>
+							<p><a href="#" class="btn btn-primary btn-sm">Read more</a></p>
+						</div>
+					</div>
+				</div>
+                <div class="col-12 col-sm-6 col-md-6 col-lg-4" data-aos="fade-up" data-aos-delay="100">
+					<div class="sermon-entry">
+						<a href="sermon-single.html"><img src="images/sq-2.jpg" alt="Image" class="img-fluid mb-3 rounded"></a>
+						<div class="sermon-body">
+							<span class="date">15 Jan 2020 <span class="mx-2">&bullet;</span> By Pastor Campbell </span>
+							<h3 class="mb-2"><a href="#">Living and Sharing The Gospel</a></h3>
+							<p class="mb-5">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia</p>
+							<p><a href="#" class="btn btn-primary btn-sm">Read more</a></p>
+						</div>
+					</div>
+				</div>
+				<div class="col-12 col-sm-6 col-md-6 col-lg-4" data-aos="fade-up" data-aos-delay="100">
+					<div class="sermon-entry">
+						<a href="sermon-single.html"><img src="images/sq-1.jpg" alt="Image" class="img-fluid mb-3 rounded"></a>
+						<div class="sermon-body">
+							<span class="date">15 Jan 2020 <span class="mx-2">&bullet;</span> By Pastor Campbell </span>
+							<h3 class="mb-2"><a href="#">Living and Sharing The Gospel</a></h3>
+							<p class="mb-5">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia</p>
+							<p><a href="#" class="btn btn-primary btn-sm">Read more</a></p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+
+
+
+</div>
+</div>
+</div>
+</div>
+	
+
+    <!-- Preloader -->
+	<div id="overlayer"></div>
+	<div class="loader">
+		<div class="spinner-border text-primary" role="status">
+			<span class="visually-hidden">Loading...</span>
+		</div>
+	</div>
+
+
+
+
+	<script src="js/bootstrap.bundle.min.js"></script>
+	<script src="js/tiny-slider.js"></script>
+	<script src="js/glightbox.min.js"></script>
+	<script src="js/aos.js"></script>
+	<script src="js/navbar.js"></script>
+	<script src="js/counter.js"></script>
+	<script src="js/custom.js"></script>
+
+	
+</body>
+</html>
